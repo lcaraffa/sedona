42a5af5115613cb977c420b045cef69143df43ca
laurent <laurent caraffa>
2024-12-22 18:04:54 +0100

    personnal script

---
+++ run_container.sh
@@ -0,0 +1,1 @@
+docker run --gpus all -u 0 --env SEDONA_SRC=/mnt/data1/code/sedona -v /mnt/data1/code/sedona:/mnt/data1/code/sedona -v /mnt/data1/datas/LidarHD:/mnt/data1/datas/LidarHD  --rm -it  sedona3d
